7361eb309241c7e7b5bd55b9bcfbe45eec01165d
amin_manna <amin10@meet.mit.edu>
2015-04-06 23:07:52 -0400

    New design for Join Screen. Roboto typeface

--- Songus/app/src/main/java/com/songus/songus/Songus.java
+++ Songus/app/src/main/java/com/songus/songus/Songus.java
@@ -1,6 +1,7 @@
 package com.songus.songus;
 
 import android.app.Application;
+import android.graphics.Typeface;
 
 /**
  * Created by amin on 4/6/15.
@@ -10,6 +11,8 @@ public class Songus extends Application {
     /*
      Global Fields Here
      */
+    public static Typeface roboto;
+    public static Typeface roboto_bold;
 
     @Override
     public void onCreate() {
@@ -17,7 +20,8 @@ public class Songus extends Application {
         /*
          Initialize here
          */
-
+        roboto = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Regular.ttf");
+        roboto_bold = Typeface.createFromAsset(getAssets(), "fonts/Roboto-Bold.ttf");
     }
 
     /*
